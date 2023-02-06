014521d19911fc68e889560fe1d0de8124701e5b
jimmy  (°O°) <117820706+jimmyomont@users.noreply.github.com>
2023-02-06 17:02:53 +0100

    Updates

---
+++ index.ad551bb3.js
@@ -0,0 +1,2 @@
+function on(){document.getElementById("jimmy").style.display="block"}function off(){document.getElementById("jimmy").style.display="none"}
+//# sourceMappingURL=index.ad551bb3.js.map
