6efc5a9bb6433e94b7e8e38c3e57f15148eafd57
julien <dev.guilbaud@gmail.com>
2023-02-06 16:27:31 +0100

    Updates

---
+++ index.a0bbd98d.js
@@ -0,0 +1,2 @@
+function on(){document.getElementById("julien").style.display="block"}function off(){document.getElementById("julien").style.display="none"}
+//# sourceMappingURL=index.a0bbd98d.js.map
