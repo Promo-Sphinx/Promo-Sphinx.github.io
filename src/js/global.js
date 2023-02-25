4be7003bb49b15386ee324fccec9e507644dbeab
julien <dev.guilbaud@gmail.com>
2023-02-25 09:07:21 +0100

    js ok

--- src/js/global.js
+++ src/js/global.js
@@ -1,4 +1,20 @@
-import feather from 'feather-icons';
 
-feather.replace();
+
+//julien
+//ciblage de la zone à cliquer
+const guilbaud = document.querySelector(".julien");
+//on écoute la zone et on lui dit quoi faire
+guilbaud.addEventListener("click",function() {
+  document.getElementById("infojulien").classList.toggle("cacher");
+})
+
+//jimmy
+//ciblage de la zone à cliquer
+const omont = document.querySelector(".jimmy");
+//on écoute la zone et on lui dit quoi faire
+omont.addEventListener("click",function() {
+  document.getElementById("infojimmy").classList.toggle("cacher");
+})
+
+  
 
--- src/js/jimmy.js
+++
@@ -1,9 +0,0 @@
-console.log('jimmy');
-function on() {
-    document.getElementById("jimmy").style.display = "block";
-  }
-  
-  function off() {
-    document.getElementById("jimmy").style.display = "none";
-  }
-  
