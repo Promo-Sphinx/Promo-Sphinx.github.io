350b655946880a53b15484fe3b13b1c3edaae9ed
julien <dev.guilbaud@gmail.com>
2023-02-26 10:33:33 +0100

    Updates

--- index.426017a1.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".julien").addEventListener("click",(function(){document.getElementById("infojulien").classList.toggle("cacher")}));document.querySelector(".jimmy").addEventListener("click",(function(){document.getElementById("infojimmy").classList.toggle("cacher")}));
-//# sourceMappingURL=index.426017a1.js.map
