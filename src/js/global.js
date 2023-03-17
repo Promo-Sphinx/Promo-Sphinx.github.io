3985164d54175d20d23b4355bd500af529d160db
jimmy  (°O°) <117820706+jimmyomont@users.noreply.github.com>
2023-03-17 07:51:13 +0100

    add background

--- src/js/global.js
+++ src/js/global.js
@@ -1,4 +1,4 @@
-
+const back = document.getElementById('back')
 
 //julien
 //ciblage de la zone à cliquer
@@ -6,64 +6,73 @@ const guilbaud = document.querySelector(".julien");
 //on écoute la zone et on lui dit quoi faire
 guilbaud.addEventListener("click",function() {
   document.getElementById("infojulien").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
 
 //jimmy
 const omont = document.querySelector(".jimmy");
 omont.addEventListener("click",function() {
   document.getElementById("infojimmy").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
-
 const kevin = document.querySelector(".kevin");
 kevin.addEventListener("click",function() {
   document.getElementById("infokevin").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
 
 const caro = document.querySelector(".caroline");
 caro.addEventListener("click",function() {
   document.getElementById("infocaro").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
 
 const quentin = document.querySelector(".quentin");
 quentin.addEventListener("click",function() {
   document.getElementById("infoquentin").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
 
 const phil = document.querySelector(".philippe");
 phil.addEventListener("click",function() {
   document.getElementById("infophilippe").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
 
 const alix = document.querySelector(".alix");
 alix.addEventListener("click",function() {
   document.getElementById("infoalix").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
 
 const elena = document.querySelector(".elena");
 elena.addEventListener("click",function() {
   document.getElementById("infoelena").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
 
 const nono = document.querySelector(".nolwenn");
 nono.addEventListener("click",function() {
   document.getElementById("infonolwenn").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
 
 const jerem = document.querySelector(".jeremy");
 jerem.addEventListener("click",function() {
   document.getElementById("infojeremy").classList.toggle("cacher");
+  back.classList.toggle("cacher");
+  
 });
 
 const david = document.querySelector(".david");
 david.addEventListener("click",function() {
   document.getElementById("infodavid").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
 
 
 const sylv = document.querySelector(".sylvain");
 sylv.addEventListener("click",function() {
   document.getElementById("infosylvain").classList.toggle("cacher");
+  back.classList.toggle("cacher");
 });
-
-  
-
